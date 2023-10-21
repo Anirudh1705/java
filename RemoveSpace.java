87f53e3672d1248ed536537135423227eeba27c0
Anirudh Kaushik <143798902+Anirudh1705@users.noreply.github.com>
2023-10-22 01:11:58 +0530

    Add files via upload

---
+++ RemoveSpace.java
@@ -0,0 +1,9 @@
+import java.util.Scanner;
+public class RemoveSpace{
+    public static void main(String[] args)
+    {
+        Scanner sc = new Scanner(System.in);
+        System.out.println("Enter the word :");
+        String word =
+    }
+}
