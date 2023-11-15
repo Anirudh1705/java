4a00962e497147208a09c87c8f2d8f135f43df2c
Anirudh Kaushik <143798902+Anirudh1705@users.noreply.github.com>
2023-11-15 18:11:56 +0530

    Add files via upload

---
+++ MainFuncOverl.java
@@ -0,0 +1,9 @@
+public class MainFuncOverl {
+    public int main(int a,int b) {
+        return (a + b);
+    }
+    public static void main(String[]args){
+        MainFuncOverl s1=new MainFuncOverl();
+        System.out.println(s1.main(3,4));
+    }
+}
