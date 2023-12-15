a28d7677ba6b06f3acd972e258c0b3052b6e2e2b
Anirudh Kaushik <143798902+Anirudh1705@users.noreply.github.com>
2023-12-15 10:08:25 +0530

    Add files via upload

---
+++ StringBuffer_Example.java
@@ -0,0 +1,7 @@
+public class StringBuffer_Example {
+    public static void main(String[]args){
+        StringBuffer s=new StringBuffer("Hello! ");
+        s.append("Anirudh Kaushik");
+        System.out.println("s refers to "+s);
+    }
+}
