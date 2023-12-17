52fb2c46ac7d8fd67da59067253f4583bdb8f0d3
Anirudh Kaushik <143798902+Anirudh1705@users.noreply.github.com>
2023-12-17 08:18:36 +0530

    Add files via upload

---
+++ String_Example.java
@@ -0,0 +1,8 @@
+public class String_Example {
+    public static void main(String[] args) {
+        String s="hello";
+        s.concat("java");
+        s=s+"java";
+        System.out.println("s refers to "+s);
+    }
+}
