935ab46b1a370e6efdaa07ba44b4ae43bf17e522
Anirudh Kaushik <143798902+Anirudh1705@users.noreply.github.com>
2023-12-31 08:30:49 +0530

    Add files via upload

---
+++ Node.java
@@ -0,0 +1,10 @@
+public class Node {
+    int data;
+    Node next;
+    Node child;
+    public Node(int data) {
+        this.data = data;
+        this.next = null;
+        this.child = null;
+    }
+}
