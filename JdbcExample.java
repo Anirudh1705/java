d02a89b17c0d29a5d836ce7f3ea8d33204c573d0
Anirudh Kaushik <143798902+Anirudh1705@users.noreply.github.com>
2024-02-04 09:11:45 +0530

    Add files via upload

---
+++ JdbcExample.java
@@ -0,0 +1,40 @@
+//Java program to set up connection and get all data from table
+import java.sql.*;
+
+public class JdbcExample {
+    public static void main(String []args)
+    {
+        Connection connection = null;
+        try {
+            // below two lines are used for connectivity.
+            Class.forName("com.mysql.cj.jdbc.Driver");
+            connection = DriverManager.getConnection(
+                    "jdbc:mysql://localhost:3306/mydb",
+                    "root", "Anirudhk@17");
+
+            // mydb is database
+            // mydbuser is name of database
+            // mydbuser is password of database
+
+            Statement statement;
+            statement = connection.createStatement();
+            ResultSet resultSet;
+            resultSet = statement.executeQuery(
+                    "select * from designation");
+            int code;
+            String title;
+            while (resultSet.next()) {
+                code = resultSet.getInt("code");
+                title = resultSet.getString("title").trim();
+                System.out.println("Code : " + code
+                        + " Title : " + title);
+            }
+            resultSet.close();
+            statement.close();
+            connection.close();
+        }
+        catch (Exception exception) {
+            System.out.println(exception);
+        }
+    } // function ends
+} // class ends
